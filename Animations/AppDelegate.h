6eb44014bf779c9e2ade4dc4ef0960df8e8568c7
devalmon <phlood@yandex.ru>
2020-05-27 08:47:13 +0300

    Initial Commit

---
+++ Animations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Animations
+//
+//  Created by Alexey Baryshnikov on 27.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
